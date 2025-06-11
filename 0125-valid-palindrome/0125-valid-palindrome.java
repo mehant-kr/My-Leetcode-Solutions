ebb341eac8d9b6afb8ed225733614b8c2b0c8ed2
Mehant Kumar <33193638+mehant-kr@users.noreply.github.com>
2025-06-11 12:38:17 -0700

    Time: 3 ms (72.26%), Space: 42.7 MB (97.76%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,27 +1,49 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        return checkPalindrome(s);
-    }
-    private boolean checkPalindrome(String s) {
-        s = cleanString(s);
-        System.out.println(s);
-        int l = s.length();
-        for (int i=0; i < l/2; i++) {
-            if (s.charAt(i) != s.charAt(l-i-1)) return false;
-        } return true;
-    }
-    // abcdefedcba
-    // 
+        // return checkPalindrome(s);
+        int first = 0;
+        int last = s.length()-1;
 
-    private String cleanString(String s) {
-        int l = s.length();
-        String str = "";
-        for (int i=0; i < l; i++) {
-            if (Character.isLetter(s.charAt(i)) || Character.isDigit(s.charAt(i))) {
-                str += Character.toLowerCase(s.charAt(i));
-
+        // a_bcdefedcba
+        while (first <= last) {
+            if (!Character.isLetterOrDigit(s.charAt(first))) {
+                first++;
             }
+            else if (!Character.isLetterOrDigit(s.charAt(last))) {
+                last--;
+            }
+            // System.out.println(first);
+            // System.out.println(last);
+            else {
+                if (Character.toLowerCase(s.charAt(first)) != Character.toLowerCase(s.charAt(last))) return false;
+                first++; last--;
+            }
+            
         }
-        return str;
+        return true;
     }
+
+
+    // private boolean checkPalindrome(String s) {
+    //     s = cleanString(s);
+    //     System.out.println(s);
+    //     int l = s.length();
+    //     for (int i=0; i < l/2; i++) {
+    //         if (s.charAt(i) != s.charAt(l-i-1)) return false;
+    //     } return true;
+    // }
+    // 
+    // // 
+
+    // private String cleanString(String s) {
+    //     int l = s.length();
+    //     String str = "";
+    //     for (int i=0; i < l; i++) {
+    //         if (Character.isLetter(s.charAt(i)) || Character.isDigit(s.charAt(i))) {
+    //             str += Character.toLowerCase(s.charAt(i));
+
+    //         }
+    //     }
+    //     return str;
+    // }
 }
