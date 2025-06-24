b7c4376214b90273ba7031021cc4da29bfe5fb66
Mehant Kumar <33193638+mehant-kr@users.noreply.github.com>
2025-06-24 09:27:10 -0700

    Time: 19 ms (1.99%), Space: 43.7 MB (6.33%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int pivot = nums[0];
+        while (left <= right) {
+            int mid = (right + left) / 2;
+            System.out.println("lmr: " + left + ", " + mid + ", " + right);
+            if (nums[mid] >= nums[0]) {
+                pivot = mid;
+                // 4 5 6 7 8 0 1
+                //.l       l ^ r
+                // [4,5,6,7,0,1,2]
+                left = mid + 1;
+            } else
+                right = mid - 1;
+        }
+        int res = binarySearch(nums, target, 0, pivot);
+        if (res == -1) {
+            res = binarySearch(nums, target, pivot + 1, nums.length - 1);
+        }
+        return res;
+
+    }
+
+    private int binarySearch(int[] nums, int target, int left, int right) {
+        System.out.println("left: " + left);
+        System.out.println("right: " + right);
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target)
+                return mid;
+            if (target < nums[mid]) {
+                right = mid - 1;
+            } else
+                left = mid + 1;
+        }
+        return -1;
+    }
+}
