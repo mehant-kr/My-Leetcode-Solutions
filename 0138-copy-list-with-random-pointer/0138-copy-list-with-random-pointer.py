f0e4eee4b9c0dec00fbfcc70fbcc270f38b8d947
Mehant Kumar <33193638+mehant-kr@users.noreply.github.com>
2025-07-10 12:14:59 -0700

    Time: 38 ms (77.44%), Space: 18.4 MB (84.29%) - LeetHub

---
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -0,0 +1,42 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if not head:
+            return None
+
+        my_dict = {}
+        while head:
+            my_dict[head] = Node(head.val)
+            head = head.next
+        
+        keys = list(my_dict.keys())
+        for i in range(len(keys) - 1):
+            my_dict[keys[i]].next = my_dict[keys[i+1]]
+            if (keys[i].random):
+                temp = my_dict[keys[i].random]
+            else:
+                temp = None
+            my_dict[keys[i]].random = temp
+
+        if keys[len(keys)-1].random:
+            # print(keys[len(keys)-1].random.val)
+            temp = my_dict[keys[len(keys)-1].random]
+        else: 
+            temp = None
+        my_dict[keys[len(keys)-1]].random = temp
+        
+        newHead = my_dict[keys[0]]
+        
+        # while newHead:
+        #     print(newHead.val)
+        #     newHead = newHead.next
+        return newHead
+        
