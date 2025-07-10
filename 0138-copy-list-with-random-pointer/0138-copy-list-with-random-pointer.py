33c44675f2359f012dfc3155f30b84d668e0b13e
Mehant Kumar <33193638+mehant-kr@users.noreply.github.com>
2025-07-10 12:36:26 -0700

    Time: 49 ms (9.53%), Space: 18.5 MB (84.37%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -9,34 +9,52 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        if not head:
-            return None
+        # if not head:
+        #     return None
 
-        my_dict = {}
-        while head:
-            my_dict[head] = Node(head.val)
-            head = head.next
+        # my_dict = {}
+        # while head:
+        #     my_dict[head] = Node(head.val)
+        #     head = head.next
         
-        keys = list(my_dict.keys())
-        for i in range(len(keys) - 1):
-            my_dict[keys[i]].next = my_dict[keys[i+1]]
-            if (keys[i].random):
-                temp = my_dict[keys[i].random]
-            else:
-                temp = None
-            my_dict[keys[i]].random = temp
+        # keys = list(my_dict.keys())
+        # for i in range(len(keys) - 1):
+        #     my_dict[keys[i]].next = my_dict[keys[i+1]]
+        #     if (keys[i].random):
+        #         temp = my_dict[keys[i].random]
+        #     else:
+        #         temp = None
+        #     my_dict[keys[i]].random = temp
 
-        if keys[len(keys)-1].random:
-            # print(keys[len(keys)-1].random.val)
-            temp = my_dict[keys[len(keys)-1].random]
-        else: 
-            temp = None
-        my_dict[keys[len(keys)-1]].random = temp
+        # if keys[len(keys)-1].random:
+        #     # print(keys[len(keys)-1].random.val)
+        #     temp = my_dict[keys[len(keys)-1].random]
+        # else: 
+        #     temp = None
+        # my_dict[keys[len(keys)-1]].random = temp
         
-        newHead = my_dict[keys[0]]
+        # newHead = my_dict[keys[0]]
         
-        # while newHead:
-        #     print(newHead.val)
-        #     newHead = newHead.next
-        return newHead
+        # # while newHead:
+        # #     print(newHead.val)
+        # #     newHead = newHead.next
+        # return newHead
+
+
+        oldToCopy = {None: None}
+        cur = head
+
+        while cur:
+            oldToCopy[cur] = Node(cur.val)
+            cur = cur.next
+
+        cur = head
+
+        while cur:
+            copy = oldToCopy[cur]
+            copy.next = oldToCopy[cur.next]
+            copy.random = oldToCopy[cur.random]
+            cur = cur.next
+
+        return oldToCopy[head]
         
